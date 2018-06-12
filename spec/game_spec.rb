aaaec328eeba659b24130f784e3471e105604f90
ky-yk-d <key0of7one0s5life@gmail.com>
2018-06-13 08:21:53 +0900

    refactor testcases

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,20 +2,25 @@ require "spec_helper"
 require "game"
 
 describe "Game" do
-  it "scores 0 in a all-gutter one" do
-    game = Game.new
-    for point in 1..20 do
-      game.roll(0)
-    end 
-    expect(game.score).to eq 0
+
+  before(:each) do
+    @game = Game.new
   end
 
-  it "scores 0 in a all-one-pin one" do
-    game = Game.new
-    for point in 1..20 do
-      game.roll(1)
-    end 
-    expect(game.score).to eq 20
+  it "scores 0 in a all-gutter one" do
+    rollMany(20,0)
+    expect(@game.score).to eq 0
+  end
+
+  it "scores 20 in a all-one-pin one" do
+    rollMany(20,1)
+    expect(@game.score).to eq 20
+  end
+
+  def rollMany(n, pins)
+    for i in 1..n do
+      @game.roll(pins)
+    end
   end
 
 end
