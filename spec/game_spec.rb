32da8ffcf41216a469520d9cd1f593bbffad4cf2
ky-yk-d <key0of7one0s5life@gmail.com>
2018-06-13 08:25:01 +0900

    fail spare test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,13 +8,21 @@ describe "Game" do
   end
 
   it "scores 0 in a all-gutter one" do
-    rollMany(20,0)
+    rollMany(20, 0)
     expect(@game.score).to eq 0
   end
 
   it "scores 20 in a all-one-pin one" do
-    rollMany(20,1)
+    rollMany(20, 1)
     expect(@game.score).to eq 20
+  end
+
+  it "scores 16 in a one-spare one" do
+    @game.roll(5)
+    @game.roll(5)
+    @game.roll(3)
+    rollMany(17, 0)
+    expect(@game.score).to eq 16
   end
 
   def rollMany(n, pins)
